cb716b359789661c12817ea18a84e28796af733a
Sandilya Kafle <kafleg@users.noreply.github.com>
2021-08-09 13:18:45 +0545

    added on git

--- functions.php
+++ functions.php
@@ -89,7 +89,7 @@ if ( !function_exists('polite_default_theme_options_values') ) :
             'polite-show-hide-date'=> 1,
             'polite-show-hide-author'=> 1,
             'polite-show-hide-read-time'=>1,
-            'polite-font-family-url'=>'Montserrat',
+            'polite-font-family-url'=>'Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap',
             'polite-blog-exclude-category'=> '',
 
             /*Single Page */
